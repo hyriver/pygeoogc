70c71fc7ad2f64db949752f30c5d2b83ebf85814
cheginit <taher.chegini@gmail.com>
2020-07-21 06:52:49 -0500

    use scm for versioning

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 """The setup script."""
-
-
+from setuptools import setup  # isort:skip
 import distutils.text_file
 from pathlib import Path
 
-from setuptools import setup
-
 
 def parse_requirements(req):
+    """Read requirements file and return it as a list."""
     return distutils.text_file.TextFile(Path(__file__).with_name(req)).readlines()
 
 
@@ -16,7 +14,8 @@ with open(Path(__file__).with_name("README.rst")) as f:
     long_description = f.read()
 
 setup(
-    version="0.1.2",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     long_description=long_description,
     long_description_content_type="text/x-rst",
     install_requires=parse_requirements("requirements.txt"),
