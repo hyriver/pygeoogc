de930d8438be206a405700f61ab4da686093a591
cheginit <cheginit@gmail.com>
2021-07-18 11:51:57 -0500

    DOC: Remove unneeded indentation [skip ci]

--- pygeoogc/utils.py
+++ pygeoogc/utils.py
@@ -419,17 +419,18 @@ def bbox_decompose(
 
         * Tuple of length 4 that represents a bounding box (west, south, east, north) of a cell,
         * A label that represents cell ID starting from bottom-left to top-right, e.g.,
-            + ------- + ------- +
-            |         |         |
-            |   0_1   |   1_1   |
-            |         |         |
-            + ------- + ------- +
-            |         |         |
-            |   0_0   |   1_0   |
-            |         |         |
-            + ------- + ------- +
+          + ------- + ------- +
+          |         |         |
+          |   0_1   |   1_1   |
+          |         |         |
+          + ------- + ------- +
+          |         |         |
+          |   0_0   |   1_0   |
+          |         |         |
+          + ------- + ------- +
         * Raster width of a cell,
-        * Raster height of a cell,
+        * Raster height of a cell
+
     """
     check_bbox(bbox)
     _bbox = match_crs(bbox, box_crs, DEF_CRS)
