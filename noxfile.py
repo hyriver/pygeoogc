89ed4066ad40736f017e6c3c204df957d80f33d9
cheginit <cheginit@gmail.com>
2021-07-24 22:03:49 -0500

    ENH: Install async_retriever from git

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,9 @@ import nox
 
 @nox.session(python="3.9")
 def tests(session):
+    hr_deps = ["async_retriever"]
+    for p in hr_deps:
+        session.install(f"git+https://github.com/cheginit/{p}.git")
     session.install(".[test]")
     session.run("pytest")
     session.run("coverage", "report")
