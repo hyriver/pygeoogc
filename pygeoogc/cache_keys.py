26ed32b8cb9a3251f82b3814ce574a519cea219f
cheginit <cheginit@gmail.com>
2022-11-26 23:54:52 -0600

    LNT: Run pre-commit [skip ci]

--- pygeoogc/cache_keys.py
+++ pygeoogc/cache_keys.py
@@ -4,7 +4,7 @@ This module is based on the `aiohttp-client-cache` package, which is
 licensed under the MIT license. See the `LICENSE` file for more details.
 """
 import hashlib
-from typing import Any, Sequence, Union, Mapping
+from typing import Any, Mapping, Sequence, Union
 
 from multidict import MultiDict
 from url_normalize import url_normalize
@@ -16,28 +16,8 @@ StrOrURL = Union[str, URL]
 __all__ = ["create_key"]
 
 
-def create_key(
-    method: str,
-    url: StrOrURL,
-    params: RequestParams = None,
-    data: Mapping[str, Any] | None = None,
-    json: Mapping[str, Any] | None = None,
-) -> str:
-    """Create a unique cache key based on request details"""
-    # Normalize and filter all relevant pieces of request data
-    norm_url = normalize_url_params(url, params)
-
-    # Create a hash based on the normalized and filtered request
-    key = hashlib.sha256()
-    key.update(method.upper().encode())
-    key.update(str(norm_url).encode())
-    key.update(encode_dict(data))
-    key.update(encode_dict(json))
-    return key.hexdigest()
-
-
 def normalize_url_params(url: StrOrURL, params: RequestParams = None) -> URL:
-    """Normalize any combination of request parameter formats that aiohttp accepts"""
+    """Normalize any combination of request parameter formats that aiohttp accepts."""
     if isinstance(url, str):
         url = URL(url)
 
@@ -53,10 +33,30 @@ def normalize_url_params(url: StrOrURL, params: RequestParams = None) -> URL:
 
 def encode_dict(data: Any) -> bytes:
     if not data:
-        return b''
+        return b""
     if isinstance(data, bytes):
         return data
     elif not isinstance(data, Mapping):
         return str(data).encode()
-    item_pairs = [f'{k}={v}' for k, v in sorted((data or {}).items())]
-    return '&'.join(item_pairs).encode()
+    item_pairs = [f"{k}={v}" for k, v in sorted((data or {}).items())]
+    return "&".join(item_pairs).encode()
+
+
+def create_key(
+    method: str,
+    url: StrOrURL,
+    params: RequestParams = None,
+    data: Mapping[str, Any] | None = None,
+    json: Mapping[str, Any] | None = None,
+) -> str:
+    """Create a unique cache key based on request details."""
+    # Normalize and filter all relevant pieces of request data
+    norm_url = normalize_url_params(url, params)
+
+    # Create a hash based on the normalized and filtered request
+    key = hashlib.sha256()
+    key.update(method.upper().encode())
+    key.update(str(norm_url).encode())
+    key.update(encode_dict(data))
+    key.update(encode_dict(json))
+    return key.hexdigest()
