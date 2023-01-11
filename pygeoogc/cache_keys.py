d9fc96f03016b2992947858fceb3ffeeabee2903
cheginit <cheginit@gmail.com>
2023-01-11 14:52:01 -0600

    DOC: Fix backticks [skip ci]

--- pygeoogc/cache_keys.py
+++ pygeoogc/cache_keys.py
@@ -1,7 +1,7 @@
 """Functions for creating unique keys based on web request parameters.
 
-This module is based on the `aiohttp-client-cache` package, which is
-licensed under the MIT license. See the `LICENSE` file for more details.
+This module is based on the ``aiohttp-client-cache`` package, which is
+licensed under the MIT license. See the ``LICENSE`` file for more details.
 """
 from __future__ import annotations
 
