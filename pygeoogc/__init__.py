65b1fda36f8ff5670833388b82eab019e2d4cf44
Taher Chegini <cheginit@gmail.com>
2024-01-08 13:46:58 -0500

    BUG: Set the path to user provided ssl cert file correctly based on https://github.com/pyproj4/pyproj/issues/1233. [skip ci]

--- pygeoogc/__init__.py
+++ pygeoogc/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for PyGeoOGC."""
+import os
 from importlib.metadata import PackageNotFoundError, version
+from pathlib import Path
 
 from pygeoogc.cache_keys import create_request_key
 from pygeoogc.exceptions import (
@@ -13,6 +15,14 @@ from pygeoogc.exceptions import (
 from pygeoogc.print_versions import show_versions
 from pygeoogc.pygeoogc import WFS, WMS, ArcGISRESTful, ServiceURL
 from pygeoogc.utils import RetrySession, match_crs, streaming_download, traverse_json, validate_crs
+
+cert_path = os.getenv("HYRIVER_SSL_CERT")
+if cert_path is not None:
+    from pyproj.network import set_ca_bundle_path
+
+    if not Path(cert_path).exists():
+        raise FileNotFoundError(cert_path)
+    set_ca_bundle_path(cert_path)
 
 try:
     __version__ = version("pygeoogc")
